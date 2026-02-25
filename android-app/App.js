566c390b48fbb8eefd2704613cc4ef7ad6ab865b
Rushi <madanerushikesh52@gmail.com>
2026-02-25 21:40:03 +0530

    Linked LoginScreen into navigation stack

--- android-app/App.js
+++ android-app/App.js
@@ -1,19 +1,6 @@
-// android-app/App.js
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./src/screens/LoginScreen";
 
-import RegisterScreen from "./src/screens/RegisterScreen";
-// We'll add LoginScreen later
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="RegisterScreen">
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+<Stack.Navigator initialRouteName="RegisterScreen">
+  <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+  <Stack.Screen name="LoginScreen" component={LoginScreen} />
+</Stack.Navigator>
