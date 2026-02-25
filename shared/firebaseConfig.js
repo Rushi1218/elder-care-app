575de04b289d3a3c635cd867c07ef6e0fe8ce11a
Rushi <madanerushikesh52@gmail.com>
2026-02-25 21:20:25 +0530

    Configured Firebase with Realtime Database support

---
+++ shared/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQQWAx5yjkb3q9HtqVl0ZYWAxE_i95GRk",
+  authDomain: "elder-care-app-337f3.firebaseapp.com",
+  databaseURL: "https://elder-care-app-337f3-default-rtdb.firebaseio.com",
+  projectId: "elder-care-app-337f3",
+  storageBucket: "elder-care-app-337f3.firebasestorage.app",
+  messagingSenderId: "803029579716",
+  appId: "1:803029579716:web:76fbfddedc48ceb99c5691"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
